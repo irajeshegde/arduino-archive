3c3112fba9f1b3b25c90f2b9d3e5159b1d5f6041
Rajesh Hegde <rajeshhegde180@gmail.com>
2021-01-05 22:28:50 +0530

    added files

---
+++ RFID/TO_READ/sketch_nov11b.ino
@@ -0,0 +1,21 @@
+#include<SoftwareSerial.h>
+//char val=0;
+SoftwareSerial mySerial(9,10);
+void setup() {
+  mySerial.begin(9600);
+Serial.begin(9600);
+
+
+}
+
+void loop() {
+if(mySerial.available()>0)
+
+//{
+// val=Serial.read();
+// Serial.print(val);}
+//}
+{Serial.write(mySerial.read());
+  }
+}
+
---
+++ Ultrasonic_CAR/Ultrasonic_CAR.ino
@@ -0,0 +1,75 @@
+int a = 7;
+int b = 8;
+int c = 9;
+int d = 10;
+
+
+#define tp 3
+#define ep 2
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(tp,OUTPUT);
+  pinMode(ep, INPUT);
+ 
+
+
+}
+
+void loop() {
+  int time;
+  float dist;
+  digitalWrite(tp,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(tp,LOW);
+  time = pulseIn(ep,HIGH);
+  dist = (time/2)/29.1;
+  
+  Serial.print(dist);
+  Serial.println("m");
+  delay(500);
+front();
+  if (dist <= 7.00) 
+  {
+    back();
+    delay(1500);
+    right();
+    delay(500);
+    front();
+ 
+  }
+
+
+}
+void front()
+{
+  digitalWrite(a, 1);
+  digitalWrite(b, 0);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+}
+void back()
+{
+  digitalWrite(a, 0);
+  digitalWrite(b, 1);
+  digitalWrite(c, 0);
+  digitalWrite(d, 1);
+}
+void left()
+{
+  digitalWrite(a, 1);
+  digitalWrite(b, 0);
+  digitalWrite(c, 0);
+  digitalWrite(d, 0);
+}
+void right()
+{
+  digitalWrite(a, 0);
+  digitalWrite(b, 1);
+  digitalWrite(c, 0);
+  digitalWrite(d, 0);
+}
